afb4b0f4b1c171c3da50e6ef010a9e65b694e0bd
Willin Wang <willin@willin.org>
2022-12-15 20:35:04 +0800

    feat: init

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='zh-CN'>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
